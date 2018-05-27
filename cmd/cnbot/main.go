12b466d8dd04cddf013b03f4fc3adb608a23cdc4
Alexey Michurin <a.michurin@gmail.com>
2018-05-27 17:41:47 +0300

    Clients factory

--- cmd/cnbot/main.go
+++ cmd/cnbot/main.go
@@ -25,6 +25,47 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!\nConfig: %#v\n", r.URL.Path[1:], h.cfg)
 }
 
+// ----------------------------------
+
+type Client struct {
+	httpClient *http.Client
+	apiURL string
+}
+
+func (c Client) PostJSON(data []byte) ([]byte, error) {
+	resp, err := c.httpClient.Post(c.apiURL, "application/json", bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return buf, nil
+}
+
+type ClientFactory struct {
+	Proxy *url.URL
+	Token string
+	Timeout time.Duration
+}
+
+func (f ClientFactory) NewClient(apiMethod string) Client {
+	return Client{
+		httpClient: &http.Client{
+			Transport: &http.Transport{
+				Proxy: func(_ *http.Request) (*url.URL, error) {  // TODO setup proxy only if proxy configured
+					return f.Proxy, nil
+				},
+			},
+			Timeout: f.Timeout,
+		},
+		apiURL: "https://api.telegram.org/bot" + f.Token + "/" + apiMethod,
+	}
+}
+
+// ----------------------------------
 
 // TODO NEXT STEPS
 // 1. Change /getMe to /getUpdates (polling loop in background)
@@ -33,23 +74,13 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // 4. Write custom wrapper for http.Client
 //    - move proxy, timeout, token[?]
 //    - methods .PostJson, .PostMultipart
-func StartPollingLoop(proxy *url.URL, token string, timeout time.Duration, pollingInterfal int, q chan<- int) {
-	httpClient := &http.Client{
-		Transport: &http.Transport{
-			Proxy: func(_ *http.Request) (*url.URL, error) {  // TODO setup proxy only if proxy configured
-				return proxy, nil
-			},
-		},
-		Timeout: timeout,
+func StartPollingLoop(client Client, pollingInterfal int, q chan<- int) {
+	buf, err := client.PostJSON([]byte("{}"))
+	if err != nil {
+		fmt.Println("ERROR", err)
+		return
 	}
-	resp, err := httpClient.Post("https://api.telegram.org/bot" + token + "/getMe", "application/json", bytes.NewReader([]byte(`{}`)))
-	// TODO check error
-	fmt.Println(err)
-	fmt.Println(resp)
-	defer resp.Body.Close()
-	b, err := ioutil.ReadAll(resp.Body)
-	// TODO check error
-	fmt.Println(string(b))
+	fmt.Println(string(buf))
 }
 
 func StartProcessor(q <-chan int, r chan<- bool) {}
@@ -57,9 +88,14 @@ func StartServer(r chan<- bool) {}
 func StartResponder(r <-chan bool) {}
 
 func StartBot(cfg config.BotConfiguration) {
+	clientFactory := ClientFactory{
+		Proxy: cfg.Proxy,
+		Token: cfg.Token,
+		Timeout: cfg.Timeout,
+	}
 	client_message_queue := make(chan int, 1000)
 	server_message_queue := make(chan bool, 1000)
-	StartPollingLoop(cfg.Proxy, cfg.Token, cfg.Timeout, cfg.PollingInterval, client_message_queue)
+	StartPollingLoop(clientFactory.NewClient("getMe"), cfg.PollingInterval, client_message_queue)
 	StartProcessor(client_message_queue, server_message_queue)  // предзапустить
 	StartServer(server_message_queue)
 	StartResponder(server_message_queue)
