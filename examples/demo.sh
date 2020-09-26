83f94d8d592f308d7a9c64b23f94c3cc4857e6f7
Alexey Michurin <a.michurin@gmail.com>
2020-09-26 14:22:39 +0300

    Demo script: use BOT_SERVER var, cosmetics

--- examples/demo.sh
+++ examples/demo.sh
@@ -4,9 +4,11 @@ set -e
 
 PATH=/usr/local/bin:/bin:/usr/bin
 
-case "CMD_$1" in # We are to say NOTCMD here. See "help" section.
+case "CMD_$1" in # We are to say NOCMD here. See "help" section.
     CMD_sup)
         echo 'Hi there'
+        ;;
+    CMD_noout)
         ;;
     CMD_nothing)
         echo '.' # Single dot is marker of silence. The bot will reply nothing.
@@ -22,19 +24,19 @@ case "CMD_$1" in # We are to say NOTCMD here. See "help" section.
         done
         ;;
     CMD_env)
-        echo '```' # Marker of Markdown preformated message
-        env
+        echo '```' # Marker of Markdown preformatted message
+        env | sort
         echo '```'
         ;;
     CMD_rose)
         convert rose: -resize 200x png:- # Just throw image to stdout as is and it will appear in the chat
         ;;
     CMD_help)
-        echo 'Available commands:' # We use "CMD"/"NOTCMD" substrings to build help message automatically
-        grep CMD_ $0 | grep -v NOTCMD | sed 's-.*CMD_-=> -;s-.$--'
+        echo 'Available commands:' # We use "CMD"/"NOCMD" substrings to build help message automatically
+        grep CMD_ $0 | grep -v NOCMD | sed 's-.*CMD_-=> -;s-.$--'
         ;;
     CMD_curl)
-        url="http://localhost:9090/$BOT_NAME/to/$BOT_FROM"
+        url="http://$BOT_SERVER/$BOT_NAME/to/$BOT_FROM"
         echo 'Message one' | curl -qfsX POST -o /dev/null --data-binary @- "$url"
         echo 'Message two' | curl -qfsX POST -o /dev/null --data-binary @- "$url"
         echo '.'
