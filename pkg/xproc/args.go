3f89b9b75c0f08530434972201b1ce7ed6ffc229
Alexey Michurin <a.michurin@gmail.com>
2026-01-17 10:27:10 +0200

    Sanitization of arguments

---
+++ pkg/xproc/args.go
@@ -0,0 +1,35 @@
+package xproc
+
+import (
+	"strings"
+	"unicode/utf8"
+)
+
+func SanitizeArgs(a []string) []string {
+	n := min(len(a), 32) // limit number of arguments
+	b := make([]string, n)
+	for i := range n {
+		if !utf8.ValidString(a[i]) { // skip invalid strings
+			continue
+		}
+		r := []rune(a[i])
+		p := make([]rune, 0, len(r))
+		for _, c := range r {
+			if c <= 0x20 || c >= 127 {
+				continue
+			}
+			switch c {
+			case '*', '?', '[', ']', // systemd: GLOB_CHARS
+				'"', '\\', '`', '$', // systemd: SHELL_NEED_ESCAPE
+				'\'', '(', ')', '<', '>', '|', '&', ';', '!': // systemd: SHELL_NEED_QUOTES
+				continue
+			}
+			p = append(p, c)
+			if len(p) >= 256 { // one argument limit
+				break
+			}
+		}
+		b[i] = strings.ToLower(string(p))
+	}
+	return b
+}
